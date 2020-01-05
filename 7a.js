ea7af36bd7f914efbd8a23190338826a5bca5339
wciurzynski <wciurzynski@gmail.com>
2020-01-05 15:16:49 +0100

    Praca nad Node.js URLDispatcher

---
+++ 7a.js
@@ -0,0 +1,19 @@
+var HttpDispatcher = require('../httpdispatcher');
+var http           = require('http');
+var dispatcher     = new HttpDispatcher();
+
+
+dispatcher.onGet("/:method", function(req, res) {
+    console.log(req.params.method);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(req.params.method);
+});
+
+dispatcher.onPost("/:method[add|sub|mul|div]", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Page Two');
+});
+
+http.createServer(function (req, res) {
+    dispatcher.dispatch(req, res);
+}).listen(3001, '0.0.0.0');
