544b5e6d4e390b049b8ac637167c355574864645
wciurzynski <wciurzynski@gmail.com>
2020-01-05 21:11:51 +0100

    Praca nad Node.js URLDispatcher

--- 7a.js
+++ 7a.js
@@ -1,15 +1,44 @@
 var HttpDispatcher = require('httpdispatcher');
 var http           = require('http');
+var url = require('url');
 var dispatcher     = new HttpDispatcher();
 
 
 dispatcher.onGet("/page1", function(req, res) {
     // console.log(req.params.method);
+
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end("page1");
 });
 
-dispatcher.onPost("/:method[add|sub|mul|div]", function(req, res) {
+dispatcher.onPost("/add", function(req, res) {
+    var parts = url.parse(req.url, true);
+    var query = parts.query;
+    var firstNumber = query.firstNumber;
+    var secondNumber = query.secondNumber;
+    if (!Number.isInteger(firstNumber)){
+        res.writeHead(400, {'Content-Type': 'text/plain'});
+        res.end('First number must be int');
+    }
+    if (!Number.isInteger(secondNumber)){
+        res.writeHead(400, {'Content-Type': 'text/plain'});
+        res.end('Second number must be int');
+    }
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(firstNumber + secondNumber);
+});
+
+dispatcher.onPost("/sub", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Page Two');
+});
+
+dispatcher.onPost("/mul", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Page Two');
+});
+
+dispatcher.onPost("/div", function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Page Two');
 });
